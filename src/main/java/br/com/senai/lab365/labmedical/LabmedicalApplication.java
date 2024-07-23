2025aef2cd6f9aa3f9a505aa64061575e9ef8a11
Felippe Kulkamp Sant ana <123035151+Felippeks@users.noreply.github.com>
2024-07-23 16:09:20 -0300

    #Lab0011/Create Project

---
+++ src/main/java/br/com/senai/lab365/labmedical/LabmedicalApplication.java
@@ -0,0 +1,13 @@
+package br.com.senai.lab365.labmedical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabmedicalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabmedicalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/senai/lab365/labmedical/LabmedicalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senai.lab365.labmedical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabmedicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
