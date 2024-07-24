6bd48065d3d9ad4a960a14ff38b8d657c8a2970d
Felippe Kulkamp Sant ana <123035151+Felippeks@users.noreply.github.com>
2024-07-24 00:11:34 -0300

    #Lab0019/Implementation Endpoits

---
+++ src/main/java/br/com/senai/lab365/labmedical/controllers/PacienteController.java
@@ -0,0 +1,59 @@
+package br.com.senai.lab365.labmedical.controllers;
+
+import br.com.senai.lab365.labmedical.entities.PacienteEntity;
+import br.com.senai.lab365.labmedical.exceptions.paciente.CpfJaCadastradoException;
+import br.com.senai.lab365.labmedical.exceptions.paciente.PacienteNaoEncontradoException;
+import br.com.senai.lab365.labmedical.services.PacienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/pacientes")
+public class PacienteController {
+    private final PacienteService pacienteService;
+
+    @Autowired
+    public PacienteController(PacienteService pacienteService) {
+        this.pacienteService = pacienteService;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> criarPaciente(@RequestBody PacienteEntity pacienteEntity) {
+        PacienteEntity paciente = pacienteService.criarPaciente(pacienteEntity);
+        return ResponseEntity.status(201).body(paciente);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<PacienteEntity>> listarPacientes(
+            @RequestParam(required = false) String nome,
+            @RequestParam(required = false) String telefone,
+            @RequestParam(required = false) String email,
+            Pageable pageable) {
+        Page<PacienteEntity> pacientes = pacienteService.listarPacientes(nome, telefone, email, pageable);
+        return ResponseEntity.ok(pacientes);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PacienteEntity> atualizarPaciente(@PathVariable Long id, @RequestBody PacienteEntity pacienteAtualizado) {
+        PacienteEntity paciente = pacienteService.atualizarPaciente(id, pacienteAtualizado);
+        return ResponseEntity.ok(paciente);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluirPaciente(@PathVariable Long id) {
+        pacienteService.excluirPaciente(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PacienteEntity> buscarPacientePorId(@PathVariable Long id) {
+        Optional<PacienteEntity> paciente = pacienteService.buscarPacientePorId(id);
+        return paciente.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+}
---
+++ src/main/java/br/com/senai/lab365/labmedical/services/PacienteService.java
@@ -0,0 +1,105 @@
+package br.com.senai.lab365.labmedical.services;
+
+import br.com.senai.lab365.labmedical.entities.PacienteEntity;
+import br.com.senai.lab365.labmedical.exceptions.paciente.CpfJaCadastradoException;
+import br.com.senai.lab365.labmedical.exceptions.paciente.PacienteNaoEncontradoException;
+import br.com.senai.lab365.labmedical.repositories.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import jakarta.persistence.criteria.Predicate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static br.com.senai.lab365.labmedical.util.ValidarCampoObrigatorio.validarCampoObrigatorio;
+
+@Service
+public class PacienteService {
+    private final PacienteRepository pacienteRepository;
+
+    @Autowired
+    public PacienteService(PacienteRepository pacienteRepository) {
+        this.pacienteRepository = pacienteRepository;
+    }
+
+    @Transactional
+    public PacienteEntity criarPaciente(PacienteEntity pacienteEntity) {
+
+        validarCampoObrigatorio(pacienteEntity.getNomeCompleto(), "nomeCompleto");
+        validarCampoObrigatorio(pacienteEntity.getGenero(), "genero");
+        validarCampoObrigatorio(pacienteEntity.getDataNascimento(), "dataNascimento");
+        validarCampoObrigatorio(pacienteEntity.getCpf(), "cpf");
+        validarCampoObrigatorio(pacienteEntity.getRg(), "rg");
+        validarCampoObrigatorio(pacienteEntity.getOrgaoExpedidorRg(), "orgaoExpedidorRg");
+        validarCampoObrigatorio(pacienteEntity.getEstadoCivil(), "estadoCivil");
+        validarCampoObrigatorio(pacienteEntity.getTelefone(), "telefone");
+        validarCampoObrigatorio(pacienteEntity.getEmail(), "email");
+        validarCampoObrigatorio(pacienteEntity.getNaturalidade(), "naturalidade");
+        validarCampoObrigatorio(pacienteEntity.getContatoEmergencia(), "contatoEmergencia");
+
+        pacienteRepository.findByCpf(pacienteEntity.getCpf())
+                .ifPresent(p -> {
+                    throw new CpfJaCadastradoException("CPF já cadastrado");
+                });
+
+        return pacienteRepository.save(pacienteEntity);
+    }
+
+    @Transactional
+    public PacienteEntity atualizarPaciente(Long id, PacienteEntity pacienteAtualizado) {
+        PacienteEntity paciente = pacienteRepository.findById(id)
+                .orElseThrow(() -> new PacienteNaoEncontradoException("Paciente não encontrado"));
+        paciente.setNomeCompleto(pacienteAtualizado.getNomeCompleto());
+        paciente.setGenero(pacienteAtualizado.getGenero());
+        paciente.setDataNascimento(pacienteAtualizado.getDataNascimento());
+        paciente.setRg(pacienteAtualizado.getRg());
+        paciente.setOrgaoExpedidorRg(pacienteAtualizado.getOrgaoExpedidorRg());
+        paciente.setEstadoCivil(pacienteAtualizado.getEstadoCivil());
+        paciente.setTelefone(pacienteAtualizado.getTelefone());
+        paciente.setEmail(pacienteAtualizado.getEmail());
+        paciente.setNaturalidade(pacienteAtualizado.getNaturalidade());
+        paciente.setContatoEmergencia(pacienteAtualizado.getContatoEmergencia());
+        paciente.setListaAlergias(pacienteAtualizado.getListaAlergias());
+        paciente.setListaCuidadosEspecificos(pacienteAtualizado.getListaCuidadosEspecificos());
+        paciente.setConvenio(pacienteAtualizado.getConvenio());
+        paciente.setNumeroConvenio(pacienteAtualizado.getNumeroConvenio());
+        paciente.setValidadeConvenio(pacienteAtualizado.getValidadeConvenio());
+        paciente.setEndereco(pacienteAtualizado.getEndereco());
+
+        return pacienteRepository.save(paciente);
+    }
+
+    @Transactional
+    public void excluirPaciente(Long id) {
+        pacienteRepository.deleteById(id);
+    }
+
+    public Optional<PacienteEntity> buscarPacientePorId(Long id) {
+        return pacienteRepository.findById(id);
+    }
+
+    public Page<PacienteEntity> listarPacientes(String nome, String telefone, String email, Pageable pageable) {
+        Specification<PacienteEntity> spec = (root, query, cb) -> {
+            List<Predicate> predicates = new ArrayList<>();
+
+            if (nome != null && !nome.isEmpty()) {
+                predicates.add(cb.like(cb.lower(root.get("nomeCompleto")), "%" + nome.toLowerCase() + "%"));
+            }
+            if (telefone != null && !telefone.isEmpty()) {
+                predicates.add(cb.equal(root.get("telefone"), telefone));
+            }
+            if (email != null && !email.isEmpty()) {
+                predicates.add(cb.equal(root.get("email"), email));
+            }
+
+            return cb.and(predicates.toArray(new Predicate[0]));
+        };
+
+        return pacienteRepository.findAll(spec, pageable);
+    }
+}
